1e08fe639c75c0af05fc260bd34c4e7fe2beb0b2
Alex Larson and Simon Temple <simonctemple@live.com>
2016-03-22 15:59:38 -0700

    add done/notdone functionality

--- app/task-list.component.ts
+++ app/task-list.component.ts
@@ -3,35 +3,39 @@ import { TaskComponent } from './task.component';
 import { Task } from './task.model';
 import { EditTaskDetailsComponent } from './edit-task-details.component';
 import { NewTaskComponent } from './new-task.component';
-
-
-console.log("task-list.component page success");
+import {DonePipe} from './done.pipe';
 
 @Component({
   selector: 'task-list',
   inputs: ['taskList'],
   outputs: ['onTaskSelect'],
+  pipes: [DonePipe],
   directives: [TaskComponent, EditTaskDetailsComponent, NewTaskComponent],
   template: `
-  <task-display *ngFor="#currentTask of taskList"
- (click)="taskClicked(currentTask)"
- [class.selected]="currentTask === selectedTask"
- [task]="currentTask">
- </task-display>
- <edit-task-details *ngIf="selectedTask" [task]="selectedTask">
- </edit-task-details>
- <new-task (onSubmitNewTask) = "createTask($event)"></new-task>
-   `
- })
+  <select (change)="onChange($event.target.value)" class = "filter">
+    <option value="all">Show All</option>
+    <option value="done">Show Done</option>
+    <option value="notDone" selected="selected">Show Not Done</option>
+  </select>
+  <task-display *ngFor="#currentTask of taskList | done:filterDone"
+    (click)="taskClicked(currentTask)"
+    [class.selected]="currentTask === selectedTask"
+    [task]="currentTask">
+  </task-display>
+  <edit-task-details *ngIf="selectedTask" [task]="selectedTask">
+  </edit-task-details>
+  <new-task (onSubmitNewTask)="createTask($event)"></new-task>
+  `
+})
 export class TaskListComponent {
   public taskList: Task[];
   public onTaskSelect: EventEmitter<Task>;
   public selectedTask: Task;
+  public filterDone: string = "notDone";
   constructor() {
     this.onTaskSelect = new EventEmitter();
   }
   taskClicked(clickedTask: Task): void {
-    console.log('child', clickedTask);
     this.selectedTask = clickedTask;
     this.onTaskSelect.emit(clickedTask);
   }
@@ -39,5 +43,8 @@ export class TaskListComponent {
     this.taskList.push(
       new Task(description, this.taskList.length)
     );
+  }
+  onChange(filterOption) {
+    this.filterDone = filterOption;
   }
 }
--- app/task.model.ts
+++ app/task.model.ts
@@ -1,6 +1,7 @@
 console.log("task page success");
 export class Task {
   public done: boolean = false;
+  // public timeStamp: any = 'null'
   constructor(public description: string, public id: number) {
   }
 }
